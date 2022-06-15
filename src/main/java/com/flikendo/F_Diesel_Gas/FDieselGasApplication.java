473104995907277417359d7d61004c88e6740860
Flikendo <jalasalavidal@gmail.com>
2022-06-15 23:25:48 +0200

    Creation of the project

---
+++ src/main/java/com/flikendo/F_Diesel_Gas/FDieselGasApplication.java
@@ -0,0 +1,13 @@
+package com.flikendo.F_Diesel_Gas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FDieselGasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FDieselGasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flikendo/F_Diesel_Gas/FDieselGasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flikendo.F_Diesel_Gas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FDieselGasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
